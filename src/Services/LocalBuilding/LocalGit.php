2339d61d9f60be6d5a24184eef6416b997ddaca7
mamazu <maxo.pech@gmail.com>
2018-02-07 00:28:28 +0100

    Added a constructor to the Github service

--- src/Services/LocalBuilding/LocalGit.php
+++ src/Services/LocalBuilding/LocalGit.php
@@ -4,9 +4,16 @@ namespace App\Services\LocalBuilding;
 
 class LocalGit implements LocalGitInterface
 {
+	private $rootDir;
+
 	public function __construct(string $rootDir)
 	{
+		$this->rootDir = $rootDir;
+	}
 
+	public function has(string $repoName)
+	{
+		$this->rootDir . DIRECTORY_SEPARATOR . base64encode($repoName);
 	}
 
 	public function clone(string $repoURL) : bool

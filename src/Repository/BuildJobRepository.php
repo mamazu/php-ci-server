507dde93994891bfd44bb1a7dc1a3fa048bf6713
mamazu <maxo.pech@gmail.com>
2018-02-25 13:55:07 +0100

    Fixed Build job repo

--- src/Repository/BuildJobRepository.php
+++ src/Repository/BuildJobRepository.php
@@ -3,38 +3,25 @@
 namespace App\Repository;
 
 use App\Entity\BuildJob;
+use App\Entity\BuildJobInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Doctrine\ORM\EntityNotFoundException;
 
 class BuildJobRepository extends ServiceEntityRepository implements BuildJobRepositoryInterface
 {
-    public function __construct(RegistryInterface $registry)
-    {
-        parent::__construct($registry, BuildJob::class);
-    }
+	public function __construct(RegistryInterface $registry)
+	{
+		parent::__construct($registry, BuildJob::class);
+	}
 
-    /** {@inheritdoc} */
-    public function getNextBuildJob()
-    {
-        $statement = $this->_em->getConnection()->prepare(<<<SQL
-        SELECT * 
-        FROM (
-            SELECT
-                MAX(id) AS id,
-                build_job_id,
-                `name` AS stateName
-            FROM
-                `build_state`
-            GROUP BY
-                build_job_id
-        ) state
-        WHERE stateName = :stateName
-        LIMIT 1;
-SQL
-);
-        $statement->execute([':stateName' => 'pending']);
-        $result = $statement->fetchAll();
-        return count($result) > 0 ? $this->find($result[0]['build_job_id']) : null;
-    }
+	/** {@inheritdoc} */
+	public function getNextBuildJob()
+	{
+		$allBuildJobs = $this->findAll();
+		$pendingJobs  = array_filter($allBuildJobs, function (BuildJobInterface $buildJob) {
+			return $buildJob->getState()->getName() === BuildJobInterface::STATUS_PENDING;
+		});
+		return count($pendingJobs) > 0 ? reset($pendingJobs) : null;
+	}
 }
